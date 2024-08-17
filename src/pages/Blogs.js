f65a407f5c9bebe927e20253d78dfa9ff5032b87
MUGADA GAYATHRI YADAV <117196335+GAYATHRI-YADAV@users.noreply.github.com>
2024-08-17 13:35:25 +0530

    Update Blogs.js

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,39 +1,3 @@
-// import React,{useState,useEffect} from 'react'
-// import axios from 'axios';
-// import BlogCard from "../components/BlogCard";
-// import './Blog.css'
-// function Blogs() {
-//   const [blogs,setBlogs] = useState([])
-//   const getAllBlogs =async () => {
-//     try{
-//       const {data} =await axios.get('/api/v1/blog/all-blogs')
-//       if(data?.success){
-//         setBlogs(data?.blogs);
-//       }
-//     }
-//     catch(error){
-//       console.log(error);
-//     }
-//   };
-//   useEffect(()=>{
-//     getAllBlogs();
-//   },[]);
-//   return (
-//     <div>
-//       {blogs && blogs.map((blog)=><BlogCard
-//          id={blog?._id}
-//          isUser={localStorage.getItem("userId") === blog?.user?._id}
-//          title={blog?.title}
-//          description={blog?.description}
-//          image={blog?.image}
-//          username={blog?.user?.username}
-//          time={blog.createdAt}
-//         />)}
-//     </div>
-//   )
-// }
-
-// export default Blogs
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import BlogCard from "../components/BlogCard";
@@ -46,7 +10,7 @@ function Blogs() {
 
   const getAllBlogs = async () => {
     try {
-      const { data } = await axios.get('https://innotech-blog-wqds.onrender.com/api/v1/blog/all-blogs');
+      const { data } = await axios.get('https://blog-backend-fono.onrender.com/api/v1/blog/all-blogs');
       if (data?.success) {
         setBlogs(data?.blogs);
       }
