b2c1ac6eec245d437283d049989a71bd80347670
MUGADA GAYATHRI YADAV <117196335+GAYATHRI-YADAV@users.noreply.github.com>
2024-08-17 23:46:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { store } from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-  <HashRouter basename='/blogfrontend'>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
